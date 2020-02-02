0f90d5b6e58a4fdef9e068eba4cf416fe86a01b0
elvira <elkinkazan@gmail.com>
2020-02-02 23:23:38 +0300

    Переименовала функцию и добавила счестчик с 0

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var TYPE_OFFER = ['palace', 'flat', 'house', 'bungalo', 'palace', 'flat', 'house', 'bungalo'];
 var CHECK_OFFER = ['12:00', '13:00', '14:00'];
 var FEATURES_OFFER = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var OBJECT_NUMBER = 9;
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var MAX_X = 1100;
 var MAX_Y = 630;
@@ -12,24 +13,25 @@ var getRandomInteger = function (max) {
   return Math.floor(Math.random() * max);
 };
 
-var generateObject = function () {
+var generateArray = function () {
   var arr = [];
-  for (var i = 1; i < 9; i++) {
+  var j = 1;
+  for (var i = 0; i < OBJECT_NUMBER; i++) {
     var currentObject = {
       author: {
-        avatar: 'img/avatars/user0' + i + '.png'
+        avatar: 'img/avatars/user0' + j + '.png'
       },
       offer: {
-        title: 'Title' + i,
+        title: 'Title' + j,
         address: '' + getRandomInteger(1000) + ',' + getRandomInteger(1000) + '',
         price: getRandomInteger(10000),
-        type: TYPE_OFFER[i - 1],
+        type: TYPE_OFFER[i],
         rooms: getRandomInteger(10),
         guests: getRandomInteger(10),
         checkin: CHECK_OFFER[getRandomInteger(CHECK_OFFER.length - 1)],
         checkout: CHECK_OFFER[getRandomInteger(CHECK_OFFER.length - 1)],
         features: FEATURES_OFFER.slice(0, getRandomInteger(FEATURES_OFFER.length - 1)),
-        description: 'Description' + i,
+        description: 'Description' + j,
         photos: PHOTOS[getRandomInteger(PHOTOS.length - 1)],
       },
       location: {
@@ -37,7 +39,8 @@ var generateObject = function () {
         y: getRandomInteger(MAX_Y - MIN_Y) + MIN_Y,
       },
     };
-    arr[i - 1] = currentObject;
+    arr[i] = currentObject;
+    j++;
   }
   return arr;
 };
@@ -48,7 +51,7 @@ var deleteClass = function (sectionName, className) {
 };
 
 var generateDOMElement = function () {
-  var currentArray = generateObject();
+  var currentArray = generateArray();
   var similarList = document.querySelector('.map__pins');
   var fragment = document.createDocumentFragment();
   var similarTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
